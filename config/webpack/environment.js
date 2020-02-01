bb025a23420b0bdf02ef977a0d9b25f1a632ce22
kruchkamgar <kruchkamgar@gmail.com>
2020-02-01 16:17:05 -0700

    add erb loader and erb-loaded secrets (API keys/tokens)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 
 // environment.config.merge({ externals: "{ mapbox-gl: 'mapbox-gl'}" }) # correct?
+
+
+environment.loaders.prepend('erb', erb)
 
 module.exports = environment
